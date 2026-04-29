432c495142c6bccdc249d3da306a17a7c2c5ff33
Okeyve <132301530+Okeyve@users.noreply.github.com>
2026-04-29 09:58:04 -0300

    Update index.js

--- index.js
+++ index.js
@@ -20,9 +20,20 @@ async function check() {
   const posts = await TikTokScraper.user(USER, { number: 1 });
   const video = posts.collector[0];
 
-  const last = await getLast();
+  const last = getLast();
 
-  if (video.id !== last) {
+if (!last) {
+  console.log('Primeira execução → enviando último vídeo');
+
+  await axios.post(WEBHOOK, {
+    content: `🧪 TESTE (último vídeo):\nhttps://www.tiktok.com/@${USER}/video/${video.id}`
+  });
+
+  saveLast(video.id);
+  return;
+}
+
+if (video.id !== last) {
     console.log('Novo vídeo detectado!');
 
     await axios.post(WEBHOOK, {
